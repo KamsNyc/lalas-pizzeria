375d607a45343db2297ad52616b97d858bdda791
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-06 17:27:56 -0400

    added Analytics for vercel

--- app/(components)/Logo.tsx
+++ app/(components)/Logo.tsx
@@ -1,15 +1,19 @@
+'use client'
 import Image from '@/node_modules/next/image'
 import React from 'react'
 
 const Logo = () => {
+
   return (
-    <div className='flex items-center justify-center'>
-        <Image
-      src="/Logo.png"
-      width={300}
-      height={300}
-      alt="Picture of the author"
-    />
+    <div
+      className='flex items-center justify-center'
+    >
+      <Image
+        src="/Logo.png"
+        width={300}
+        height={300}
+        alt="Picture of the author"
+      />
     </div>
   )
 }
