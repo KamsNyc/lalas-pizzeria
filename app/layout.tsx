c492f35034c75f3aceb569b08017b2aeb3389be4
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-24 17:37:55 -0400

    added slice script

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function (w, d, s, o, f, js, fjs) {
+                w[o] = w[o] || function (opt) { w[o].q = w[o].q || opt };
+                (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+                js.src = f; js.async = 1;
+                if (fjs) { fjs.parentNode.insertBefore(js, fjs) }
+                else {
+                  fjs = d.getElementsByTagName('head')[0];
+                  fjs.appendChild(js)
+                }
+              })(window, document, 'script', 'SliceWidgets', 'https://restaurant-widgets-integrations.slicelife.com/widget.js');
+              SliceWidgets({ shopUuid: '965c6ebe-0a09-4e29-bf6c-4954b8773a53' });
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <header className=''> <Navbar /></header>
         <main>
